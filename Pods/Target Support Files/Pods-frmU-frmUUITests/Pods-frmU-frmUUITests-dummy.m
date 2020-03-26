7ef0dd5e357b7d09fd23829e6c03eab41c370ad0
bufudash <alanzhu@berkeley.edu>
2020-03-26 02:36:40 -0700

    ignore pods

--- Pods/Target Support Files/Pods-frmU-frmUUITests/Pods-frmU-frmUUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_frmU_frmUUITests : NSObject
-@end
-@implementation PodsDummy_Pods_frmU_frmUUITests
-@end
--- Pods/Target Support Files/Pods-frmUTests/Pods-frmUTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_frmUTests : NSObject
-@end
-@implementation PodsDummy_Pods_frmUTests
-@end
